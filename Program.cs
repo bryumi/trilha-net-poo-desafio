de496f7aed94403c36370b0b14e9f39b470899cf
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-12-04 14:31:38 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone meuSmartphoneApple = new Iphone("123456789", "iPhone 13", "5458", 32);
+Nokia meuNokia = new Nokia("101213142", "Nokia 13", "222333", 64);
+
+            // Chamando o método Ligar da classe Smartphone
+meuSmartphoneApple.Ligar();
+meuSmartphoneApple.InstalarAplicativo("DIO");
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("Linkedin");
+
+Console.ReadLine();
